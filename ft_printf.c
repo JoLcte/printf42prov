bc01f2abfdb21cb719307f22d6aa212142b18f67
Johanna LECOMTE <jlecomte@e2r5p2.42.fr>
2021-02-07 17:15:23 +0100

    norm not ok but types and conv ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,29 +6,17 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 16:17:53 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/05 14:59:47 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/07 17:14:53 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-#include <unistd.h>
 #include "libftprintf.h"
 
-/*
- ** DRAFT VERSION
- */
-
-
-void ft_cpy(char *dst, char *src, size_t len)
+size_t	buf_pilot(char *buf, const char *s, size_t len, size_t len_buf)
 {
-	while (len--)
-		*dst++ = *src++;
-}
+	size_t	rest_len;
 
-int	buf_pilot(char *buf, char *s, int len, size_t len_buf)
-{
-	size_t rest_len = BUFSIZ - len_buf;
+	rest_len = BUFSIZ - len_buf;
 	while (len > rest_len)
 	{
 		ft_cpy(buf + len_buf, s, rest_len);
@@ -41,38 +29,36 @@ int	buf_pilot(char *buf, char *s, int len, size_t len_buf)
 	if (len)
 		ft_cpy(buf + len_buf, s, len);
 	len_buf += len;
-}
-return (len_buf);
+	return (len_buf);
 }
 
-int	ft_printf(const char *str, ...)
+int		ft_printf(const char *str, ...)
 {
-	char buf[BUFSIZ];
-	int char_count;
-	char *s;
-	size_t len_buf;
-	va_list ap;
+	char		buf[BUFSIZ];
+	int			char_count;
+	const char	*s;
+	size_t		len_buf;
+	va_list		ap;
 
 	char_count = 0;
 	len_buf = 0;
-	s = /*(char)*/str;
+	s = str;
 	if (!str && !*str)
 	{
-		write(1, "(null)", 6); 
+		write(1, "(null)", 6);
 		return (0);
 	}
 	va_start(ap, str);
 	while (*str)
 	{
+		// till '%'
 		s = char_chr(str);
 		char_count = s - str;
 		len_buf = buf_pilot(buf, str, char_count, len_buf);
-		//dans le cas ou *s = 0 ->;
-		if (*s)
-		//if faut qu'on reboucle a partir de str qui sera egal a apres le type de conv ou apres le % si echec
-		{
+
+		//from '%' : conv
+		if (*s) // comme ca si *s = 0 on ne rentre pas dedans
 			char_count += parse_conv(&s, ap, len_buf, buf);
-		}
 		str = s;
 	}
 	if (*buf)
