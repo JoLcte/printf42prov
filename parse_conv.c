152cd2cc5a3d618d83336da63064a4558065e89d
Johanna LECOMTE <jlecomte@e2r5p2.42.fr>
2021-02-07 16:26:58 +0100

    change order in parse_conv

--- parse_conv.c
+++ parse_conv.c
@@ -6,120 +6,119 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 12:30:02 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/05 16:49:37 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/07 16:26:20 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libftprintf.h"
 
-void	bonus_parse(const char **s, t_flags *f)
+static t_flags	*t_init(t_flags *f)
 {
+	f->fzero = 0;
+	f->fleft = 0;
+	f->fplus = 0;
+	f->fspace = 0;
+	f->prefix = 0;
+	f->width = -1;
+	f->prec = -1;
+	return (f);
+}
+
+void			bonus_parse(const char **str, t_flags *f)
+{
+	const char *s = *str;
+
 	while (1)
 	{
-		if (**s == '#')
+		if (*s == '#')
 		{
 			f->prefix = 1;
-			(*s)++;
+			s++;
 		}
-		else if (**s == '+')
+		else if (*s == '+')
 		{
 			f->fplus = 1;
-			(*s)++;
+			s++;
 			if (f->fspace)
 				f->fspace = 0;
 		}
-		else if (**s == ' ' && !f->fplus)
+		else if (*s == ' ' && !f->fplus)
 		{
 			f->fspace = 1;
-			(*s)++;
+			s++;
 		}
 		else
-			break;
+			break ;
 	}
 }
 
-void width_n_prec(const char **str, t_flags *f)
+void			width_n_prec(const char **str, t_flags *f, va_list ap)
 {
-	const char *s = str;
+	const char *s = *str;
+
 	while (1)
 	{
 		if (*s == '*')
-		{
-			f->width = va_arg(ap, int);
-			s++;
-		}
-		else if (*s >= '0' && <= '9')
-			f->witdh = ft_atoi(s);
+			neg_width(ap, f, &s);
+		else if (*s >= '0' && *s <= '9')
+			f->width = ft_atoi(&s);
 		else if (*s == '.')
 		{
 			s++;
 			if (*s == '*')
-			{
-				f->prec = va_arg(ap, int);
-				if (f->prec < 0)
-					f->prec = -(f->prec);
-			}
+				neg_prec(ap, f, &s);
 			else
-				f->prec = ft_atoi(s);
+			{
+				f->prec = ft_atoi(&s);
+				f->fzero = 0;
+			}
 		}
 		else
-			break;
+			break ;
 	}
 	*str = s;
 }
 
-void	flags_parse(const char **s, t_flags *f)
+void			flags_parse(const char **str, t_flags *f, va_list ap)
 {
+	const char *s = *str;
+
 	while (1)
 	{
-		if (**s == '0' && !(f->fleft))
+		if (*s == '0' && !(f->fleft))
 		{
 			f->fzero = 1;
-			(*s)++;
+			s++;
 		}
-		else if (**s == '-')
+		else if (*s == '-')
 		{
-			while (**s == '-')
-				(*s)++;
+			while (*s == '-')
+				s++;
 			f->fleft = 1;
 			if (f->fzero)
 				f->fzero = 0;
 		}
 		else
-			break;
+			break ;
 	}
-	bonus_parse(s, f);
-	width_n_prec(s, f);
+	bonus_parse(&s, f);
+	width_n_prec(&s, f, ap);
 }
 
-int	padd_conv(const char **s, va_list ap, int len_buf, char *buf, t_flags *f)
+int				parse_conv(const char **s, va_list ap, int len_buf, char *buf)
 {
-	char *s;
+	t_flags *f;
+	int		len;
 
-	s = *s;
-	if (*s == 'c' || *s == 's')
-		return (str_conv(va_arg(ap, const char *), len_buf, buf, f));
-	else if (*s == 'p')
-		return (ptr_conv(va_arg(ap, void *), len_buf, buf, f));
-	else if (*s == 'd' || *s == 'i' || *s == 'u'
-			return (numsign_conv(va_arg(ap, int), len_buf, buf, f));
-			else if (*s == 'o' || *s == 'x' || *s == 'X')
-			return (numunsign_conv(va_arg(ap, unsigned int), len_buf, buf, f));
-			else
-			return (0);
-			}
-
-			int	parse_conv(const char **s, va_list ap, int len_buf, char *buf)
-			{
-			t_flags *f;
-			int len;
-
-			if (*(*s + 1) == '%')
-			truc;
-			*f = (t_flags){0, 0, 0, 0, 0, -1, -1};
-			flag_parse(s, f);
-			len = padd_conv(s, ap, len_buf, buf, f);
-			*s++; // pour ne plus etre sur le type
-			return (len);
-			}
+	if (*(*s + 1) == '%')
+	{
+		len_buf = buf_pilot(buf, (char *)(*s)++, 1, len_buf);
+		return (1);
+	}
+	f = t_init(f);
+	flags_parse(s, f, ap);
+	len = padd_conv(s, ap, len_buf, buf, f);
+	(*s)++;
+	return (len);
+}
