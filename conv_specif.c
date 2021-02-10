d5573957ca0ae0cc379235d765e3f79a33320afa
Johanna LECOMTE <jlecomte@e2r7p2.42.fr>
2021-02-10 16:54:01 +0100

    spec str nightmaire

--- conv_specif.c
+++ conv_specif.c
@@ -6,22 +6,51 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 16:17:32 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/07 16:00:54 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/10 16:53:40 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	str_conv(const char *s, size_t len_buf, char *buf, t_flags *f)
+int	str_conv(va_list ap. size_t len_buf, char *buf, t_flags *f)
 {
+	char *s_conv;
+	int len_s;
+
+	s_conv = va_arg(ap, char *);
+	len_s = ft_strlen(s_conv);
+	if (f->prec > len_s)
+	{
+		s_conv[f->prec] = 0;
+		len_s = ft_strlen(s_conv);
+	}
+	f->width -= len_s;
+	if (f->width > 0)
+	{
+		if (f->fleft)
+		{
+			len_buf = buf_pilot(buf, s_conv, len_s, len_buf);
+			len_buf = buf_pilot(buf, some_magic("    ", f->width, len_buf);
+		}
+		else
+		{
+		len_buf = buf_pilot(buf, some_magic("    ", f->width, len_buf);
+				len_buf = buf_pilot(buf, s_conv, len_s, len_buf);
+		}
+		return (len_s + f->width); // CAS AVEC LEN_S > WIDTH ? Marche pas
+// PB : LEN_BUF NE VA PAS ETRE RETURNED
+	}
+
 	// creer un char * qui serait modifie avec str;
 //appel a buf_pilot
 // return len_final
 }
 
-int ptr_conv(void *p, size_t len_buf, char *buf, t_flags *f)
+int ptr_conv(void *p, va_list ap, size_t len_buf, char *buf, t_flags *f)
 {
-
+//ajouter 0x ?
+// faire des tests sur un char * par exemple
+// ou un *int
 }
 
 int numsign_conv(int num, size_t len_buf, char *buf, t_flags *f)
