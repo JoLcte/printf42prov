0935c55bcd8f4030cafabbc70e32f9f01912c1c4
Johanna LECOMTE <jlecomte@e2r7p2.42.fr>
2021-02-10 16:52:45 +0100

    %s tests

--- main_rprintf.c
+++ main_rprintf.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 
-int main(int ac, char **av)
+int main(/*int ac, char **av*/)
 {
 	//int a = ;
 	int res = 0;
 
-	if (ac == 3)
-	{
-		//return (printf("|%s|\n", ""));
-		res = printf(av[1], av[2]);
-	}
-	printf("%d\n", res);
+	/*if (ac == 4)
+	{*/
+		res = printf("|%-7.12s|", "coucou bonjour salut");
+	//	res = printf(av[1], atoi(av[2]), atoi(av[3]));
+	//}
+	printf("\n%d\n", res);
 	return (0);
 }
