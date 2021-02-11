557c657e543772bb71927cf066623cb9af9b8c72
Johanna LECOMTE <jlecomte@e1r9p10.42.fr>
2021-02-11 16:56:24 +0100

    space_magic

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 16:20:39 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/10 15:12:50 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/11 16:56:09 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	neg_width(va_list ap, t_flags *f, const char **str)
 {
-	f->width = va_arg(ap, int);
+	f->width = va_arg(ap, size_t);
 	if (f->width < 0)
 	{
 		f->width *= -1;
@@ -28,8 +28,19 @@ void	neg_width(va_list ap, t_flags *f, const char **str)
 
 void	neg_prec(va_list ap, t_flags *f, const char **str)
 {
-	f->prec = va_arg(ap, int);
+	f->prec = va_arg(ap, size_t);
 	if (f->prec < 0)
 		f->prec = *= -1;
 	++(*s);
+}
+
+char	*space_magic(size_t n)
+{
+	char s[] = n;
+	char *ptr_s;
+
+	ptr_s = s;
+	while (n--)
+		*ptr_s++ = ' ';
+	return (s);
 }
