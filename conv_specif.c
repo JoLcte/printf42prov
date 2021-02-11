c73414de91a61bf2589e5c83a4cf59f0cd4f027a
Johanna LECOMTE <jlecomte@e1r9p10.42.fr>
2021-02-11 16:58:57 +0100

    str_conv and &len_buf

--- conv_specif.c
+++ conv_specif.c
@@ -6,19 +6,22 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 16:17:32 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/10 16:53:40 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/11 16:58:37 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	str_conv(va_list ap. size_t len_buf, char *buf, t_flags *f)
+int	str_conv(va_list ap. size_t *len_buf, char *buf, t_flags *f)
 {
 	char *s_conv;
-	int len_s;
+	size_t len_s;
+	char *s_width;
+	size_t l_buf;
 
 	s_conv = va_arg(ap, char *);
 	len_s = ft_strlen(s_conv);
+	l_buf = *len_buf;
 	if (f->prec > len_s)
 	{
 		s_conv[f->prec] = 0;
@@ -27,54 +30,52 @@ int	str_conv(va_list ap. size_t len_buf, char *buf, t_flags *f)
 	f->width -= len_s;
 	if (f->width > 0)
 	{
+		s_width = space_magic(f->width);
 		if (f->fleft)
 		{
-			len_buf = buf_pilot(buf, s_conv, len_s, len_buf);
-			len_buf = buf_pilot(buf, some_magic("    ", f->width, len_buf);
+			l_buf = buf_pilot(buf, s_conv, len_s, l_buf);
+			l_buf = buf_pilot(buf, s_width, f->width, l_buf);
 		}
 		else
 		{
-		len_buf = buf_pilot(buf, some_magic("    ", f->width, len_buf);
-				len_buf = buf_pilot(buf, s_conv, len_s, len_buf);
+			l_buf = buf_pilot(buf, s_width, f->width, l_buf);
+			l_buf = buf_pilot(buf, s_conv, len_s, l_buf);
 		}
-		return (len_s + f->width); // CAS AVEC LEN_S > WIDTH ? Marche pas
-// PB : LEN_BUF NE VA PAS ETRE RETURNED
+		return ((int)(len_s) + (int)f->width);
 	}
-
-	// creer un char * qui serait modifie avec str;
-//appel a buf_pilot
-// return len_final
+	return (f->width);
+	//substitute functions inside to make it shorter
 }
 
-int ptr_conv(void *p, va_list ap, size_t len_buf, char *buf, t_flags *f)
+int ptr_conv(void *p, va_list ap, size_t *len_buf, char *buf, t_flags *f)
 {
-//ajouter 0x ?
-// faire des tests sur un char * par exemple
-// ou un *int
+	//ajouter 0x ?
+	// faire des tests sur un char * par exemple
+	// ou un *int
 }
 
-int numsign_conv(int num, size_t len_buf, char *buf, t_flags *f)
+int numsign_conv(int num, size_t *len_buf, char *buf, t_flags *f)
 {
 
 }
 
-int numunsign_conv(unsigned int num, size_t len_buf, char *buf, t_flags *f)
+int numunsign_conv(unsigned int num, size_t *len_buf, char *buf, t_flags *f)
 {
 
 }
 
-int	padd_conv(const char **str, va_list ap, int len_buf, char *buf, t_flags *f)
+int	padd_conv(const char **str, va_list ap, *size_t len_buf, char *buf, t_flags *f)
 {
 	const char *s = *str;
 
 	if (*s == 'c' || *s == 's')
-		return (str_conv(va_arg(ap, const char *), len_buf, buf, f));
+		return (str_conv(va_arg(ap, const char *), &len_buf, buf, f));
 	else if (*s == 'p')
-		return (ptr_conv(va_arg(ap, void *), len_buf, buf, f));
+		return (ptr_conv(va_arg(ap, void *), &len_buf, buf, f));
 	else if (*s == 'd' || *s == 'i' || *s == 'u')
-		return (numsign_conv(va_arg(ap, int), len_buf, buf, f));
+		return (numsign_conv(va_arg(ap, int), &len_buf, buf, f));
 	else if (*s == 'o' || *s == 'x' || *s == 'X')
-		return (numunsign_conv(va_arg(ap, unsigned int), len_buf, buf, f));
+		return (numunsign_conv(va_arg(ap, unsigned int), &len_buf, buf, f));
 	else
 		return (0);
 }
