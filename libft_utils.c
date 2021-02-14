54ecc082f5388cd6818af2a68d161f3e066a667f
Johanna LECOMTE <jlecomte@e2r4p2.42.fr>
2021-02-14 16:55:27 +0100

    ft_atoi complete

--- libft_utils.c
+++ libft_utils.c
@@ -6,16 +6,15 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 13:38:50 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/02/07 17:09:54 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/14 15:17:23 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 #include "libftprintf.h"
 
-char	*char_chr(const char *s)
+const char	*char_chr(const char *s)
 {
-	char *const save = s;
-
 	while (*s)
 	{
 		if (*s == '%')
@@ -25,7 +24,7 @@ char	*char_chr(const char *s)
 	return (s);
 }
 
-size_t	ft_strlen(char *s)
+size_t		ft_strlen(char *s)
 {
 	const char *const save = s;
 
@@ -34,12 +33,31 @@ size_t	ft_strlen(char *s)
 	return (s - save);
 }
 
-void	ft_cpy(char *dst, const char *src, size_t len)
+void		ft_cpy(char *dst, const char *src, size_t len)
 {
 	while (len--)
 		*dst++ = *src++;
 }
 
-int		ft_atoi(const char *s)
+int		ft_atoi(const char **str)
 {
+	const char *s = *str;
+
+	long int	res;
+	int			neg;
+
+	res = 0;
+	neg = 1;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			neg *= -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+		res = res * 10 + *s++ - '0';
+	*str = s;
+	return ((int)(res * neg));
 }
