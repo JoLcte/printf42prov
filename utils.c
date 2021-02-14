a2bea00cfd86ebd624332e5790e83362a75c901c
Johanna LECOMTE <jlecomte@e2r4p2.42.fr>
2021-02-14 16:55:08 +0100

    un peu de repeat dans la magic

--- utils.c
+++ utils.c
@@ -5,42 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/14 09:46:41 by jlecomte          #+#    #+#             */
+/*   Updated: 2021/02/14 16:14:30 by jlecomte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 16:20:39 by jlecomte          #+#    #+#             */
 /*   Updated: 2021/02/11 16:56:09 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 #include "libftprintf.h"
 
-void	neg_width(va_list ap, t_flags *f, const char **str)
+void	neg_width(va_list ap, t_flags *f, const char **s)
 {
 	f->width = va_arg(ap, size_t);
 	if (f->width < 0)
 	{
 		f->width *= -1;
-		if (f->left = 1)
-			f->left = 0;
+		if (f->fleft == 1)
+			f->fleft = 0;
 		else
-			f->left = 1;
+			f->fleft = 1;
 	}
 	++(*s);
 }
 
-void	neg_prec(va_list ap, t_flags *f, const char **str)
+void	neg_prec(va_list ap, t_flags *f, const char **s)
 {
 	f->prec = va_arg(ap, size_t);
 	if (f->prec < 0)
-		f->prec = *= -1;
+		f->prec *= -1;
 	++(*s);
 }
 
-char	*space_magic(size_t n)
+char	*space_magic(char *s, size_t n)
 {
-	char s[] = n;
+	printf("on est entre dans space magic et n = %zu\n", n);
 	char *ptr_s;
 
+	s[n] = 0;
 	ptr_s = s;
 	while (n--)
+	{
+		printf("n = %zu\n", n);
 		*ptr_s++ = ' ';
+	}
 	return (s);
 }
