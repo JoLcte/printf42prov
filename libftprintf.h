602277a13402379f75959bacae31b742bfae096e
Jo Lecomte <Shlurck@shlurck.home>
2021-02-20 10:51:27 +0100

    change str_conv rm *f param

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: JoLecomte <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 17:35:00 by JoLecomte         #+#    #+#             */
-/*   Updated: 2021/02/18 17:12:16 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/02/20 10:41:22 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ typedef struct	s_flags
 */
 
 int				ft_printf(const char *str, ...);
-void			buf_pilot(char *buf, const char *s, size_t len);
+size_t			buf_pilot(char *buf, const char *s, size_t len);
 
 /*
 ** PARSING FUNCTIONS
@@ -56,7 +56,8 @@ int				parse_conv(const char **s, va_list ap, char *buf);
 
 int				conv_str(va_list ap, char *buf, t_flags *f);
 int				conv_num(va_list ap, char *buf, t_flags *f);
-int				str_conv(char *s, size_t len_s, char *buf, t_flags *f);
+int				str_conv_width(char *s, size_t len_s, char *buf, t_flags *f);
+int				str_conv(char *s, size_t len_s, char *buf);
 int				ptr_conv(void *p, char *buf, t_flags *f);
 int				numsign_conv(int num, char *buf, t_flags *f);
 int				numunsign_conv(unsigned int num, char *buf,
